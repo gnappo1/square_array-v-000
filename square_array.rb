f49b83d1a66664a9f16906eb31d326e183e83341
Matteo Piccini <gnappo.1@live.it>
2020-03-10 21:47:11 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,9 +1,8 @@
 def square_array(array)
-  # new_array = []
-  # array.each do |i|
-  #   i = i*i
-  #   new_array.push(i)
-  # end
-  # new_array
-  array.map {|el| el**2}
+  new_array = []
+  array.each do |i|
+    i = i*i
+    new_array.push(i)
+  end
+  new_array
 end
